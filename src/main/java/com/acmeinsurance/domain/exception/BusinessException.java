685232a3019ded964d75d2ebb24fa44547bb488f
Ana Alves <barbosaalvesanabeatriz+github@gmail.com>
2025-04-13 16:00:22 -0300

    feat: implement global exception handler with standard error model

---
+++ src/main/java/com/acmeinsurance/domain/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.acmeinsurance.domain.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+}
